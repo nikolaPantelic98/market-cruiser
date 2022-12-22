1b66eb88c59f64dc4716272c6d37c09c184aa79f
nikolaPantelic98 <nikolapantelic998@gmail.com>
2022-12-22 16:50:51 +0100

    Category Form

--- market-cruiser-web-parent/market-cruiser-back-end/src/main/java/com/marketcruiser/admin/category/CategoryController.java
+++ market-cruiser-web-parent/market-cruiser-back-end/src/main/java/com/marketcruiser/admin/category/CategoryController.java
@@ -18,11 +18,25 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    // shows a list of categories
     @GetMapping("/categories")
     public String showCategories(Model model) {
         List<Category> listCategories = categoryService.getAllCategories();
         model.addAttribute("listCategories", listCategories);
 
         return "categories/categories";
+    }
+
+    // shows a form for creating a new category
+    @GetMapping("/categories/new")
+    public String newCategory(Model model) {
+        Category category = new Category();
+        List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+
+        model.addAttribute("category", category);
+        model.addAttribute("listCategories", listCategories);
+        model.addAttribute("pageTitle", "Create New Category");
+
+        return "categories/category_form";
     }
 }
--- market-cruiser-web-parent/market-cruiser-back-end/src/main/java/com/marketcruiser/admin/category/CategoryServiceImpl.java
+++ market-cruiser-web-parent/market-cruiser-back-end/src/main/java/com/marketcruiser/admin/category/CategoryServiceImpl.java
@@ -4,7 +4,9 @@ import com.marketcruiser.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CategoryServiceImpl implements CategoryService{
@@ -18,8 +20,52 @@ public class CategoryServiceImpl implements CategoryService{
     }
 
 
+    // returns a list of categories in the database
     @Override
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
+    }
+
+    // returns a list of categories in the database which includes all top-level categories and their children, with each subcategory
+    @Override
+    public List<Category> listCategoriesUsedInForm() {
+        List<Category> categoriesUsedInForm = new ArrayList<>();
+        List<Category> categoriesInDB = categoryRepository.findAll();
+
+        for (Category category : categoriesInDB) {
+            if (category.getParent() == null) {
+                categoriesUsedInForm.add(new Category(category.getName()));
+
+                Set<Category> children = category.getChildren();
+
+                for (Category subCategory : children) {
+                    String name = "--" + subCategory.getName();
+                    categoriesUsedInForm.add(new Category(name));
+
+                    listChildren(categoriesUsedInForm, subCategory, 1);
+                }
+            }
+        }
+
+        return categoriesUsedInForm;
+    }
+
+    // adds the children of a given category to a list of categories used in a form
+    // the subcategories are indented by a number of hyphens to indicate their level in the hierarchy
+    private void listChildren (List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+        int newSubLevel = subLevel + 1;
+        Set<Category> children = parent.getChildren();
+
+        for (Category subCategory : children) {
+            String name = "";
+            for (int i = 0; i < newSubLevel; i++) {
+                name += "--";
+            }
+
+            name += subCategory.getName();
+            categoriesUsedInForm.add(new Category(name));
+
+            listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+        }
     }
 }
