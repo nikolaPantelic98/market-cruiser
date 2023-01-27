2caf9a6d1bd6c39fabd7d18c216d3578adb6e36f
nikolaPantelic98 <nikolapantelic998@gmail.com>
2023-01-27 23:22:04 +0100

    Add Extra Images on Product Form

--- market-cruiser-web-parent/market-cruiser-back-end/src/main/resources/static/js/common_form.js
+++ market-cruiser-web-parent/market-cruiser-back-end/src/main/resources/static/js/common_form.js
@@ -4,16 +4,11 @@ $(document).ready(function() {
     });
 
     $("#fileImage").change(function() {
-        fileSize = this.files[0].size;
-
-        if (fileSize > 1048576) {
-            this.setCustomValidity("You must choose an image less than 1 MB");
-            this.reportValidity();
-        } else {
-            this.setCustomValidity("");
-            showImageThumbnail(this);
+        if (!checkFileSize(this)) {
+            return;
         }
 
+        showImageThumbnail(this);
     });
 });
 
@@ -25,6 +20,21 @@ function showImageThumbnail(fileInput) {
     };
 
     reader.readAsDataURL(file);
+}
+
+function checkFileSize(fileInput) {
+    fileSize = fileInput.files[0].size;
+
+    if (fileSize > 1048576) {
+        fileInput.setCustomValidity("You must choose an image less than 1 MB");
+        fileInput.reportValidity();
+
+        return false;
+    } else {
+        fileInput.setCustomValidity("");
+
+        return true;
+    }
 }
 
 function showModalDialog(title, message) {
