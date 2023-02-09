311044dbffa26bf57bfe7012b7e3807bbccaa75f
nikolaPantelic98 <nikolapantelic998@gmail.com>
2023-02-09 22:50:53 +0100

    Home Page for Shopping Application

--- market-cruiser-web-parent/market-cruiser-front-end/src/main/java/com/marketcruiser/MainController.java
+++ market-cruiser-web-parent/market-cruiser-front-end/src/main/java/com/marketcruiser/MainController.java
@@ -1,4 +1,4 @@
-package com.marketcruiser.site;
+package com.marketcruiser;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- market-cruiser-web-parent/market-cruiser-front-end/src/main/java/com/marketcruiser/MarketCruiserFrontEndApplication.java
+++ market-cruiser-web-parent/market-cruiser-front-end/src/main/java/com/marketcruiser/MarketCruiserFrontEndApplication.java
@@ -1,4 +1,4 @@
-package com.marketcruiser.site;
+package com.marketcruiser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
